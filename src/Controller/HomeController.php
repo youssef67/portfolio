ad623ba9a2e29723a92484449e16dd3bf76600b4
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-06-17 16:29:34 +0200

    download cv + mail + design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/home.html.twig', [
+            'route' => 'home'
+        ]);
+    }
+
+    /**
+     * @Route("/resume", name="resume")
+     */
+    public function resume()
+    {
+        return $this->render('home/resume.html.twig', [
+            'route' => 'resume'
+        ]);
+    }
+
+    /**
+     * @Route("/download", name="download_resume")
+     */
+    public function downloadResume()
+    {
+        $response = new BinaryFileResponse(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'public/images/cv-youssef-moudni.pdf');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'cv-youssef-moudni.pdf');
+        return $response;
     }
 }
