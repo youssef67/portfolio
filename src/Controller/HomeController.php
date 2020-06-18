10225de06b853b4504c2af35dc79722c2297e878
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-06-18 19:52:20 +0200

    finalisation de la partie front-end

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -31,12 +31,34 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/download", name="download_resume")
+     * @Route("/download/{file}", name="downloadFile", methods="GET")
      */
-    public function downloadResume()
+    public function downloadFile(Request $request)
     {
-        $response = new BinaryFileResponse(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'public/images/cv-youssef-moudni.pdf');
+        $response = new BinaryFileResponse(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'public/download/' . $request->get('file'));
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'cv-youssef-moudni.pdf');
         return $response;
     }
+
+     /**
+     * @Route("/realisations", name="allProjects")
+     */
+    public function allProjects()
+    {
+        return $this->render('projects/projects.html.twig', [
+            'route' => 'projects'
+        ]);
+    }
+
+     /**
+     * @Route("/{project}", name="project")
+     */
+    public function project($project)
+    {
+        return $this->render('projects/'. $project .'.html.twig', [
+            'route' => 'projects'
+        ]);
+    }
+
+
 }
